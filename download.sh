7000f3598064f071a6ec49bd2abca7f6d7f55c1f
Mathieu Méa <mathieu.mea@gmail.com>
2015-06-30 18:32:27 -0400

    Check Last-Modified before downloading GTFS file.

--- download.sh
+++ download.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 echo ">> Downloading..."
-# wget -i input_url -O input/gtfs.zip
-# wget --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11" --header="Referer: http://xmodulo.com/" --header="Accept-Encoding: compress, gzip" -i input_url -O input/gtfs.zip
-# wget --header="User-Agent: Mozilla/5.0 (Windows NT 6.0) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.97 Safari/537.11" -i input_url -O input/gtfs.zip
-wget --header="User-Agent: MonTransit" -i input_url -O input/gtfs.zip
+URL=`cat input_url`;
+FILENAME=$(basename "$URL");
+if [ -e input/gtfs.zip ]; then
+    mv input/gtfs.zip $FILENAME;
+    wget --header="User-Agent: MonTransit" -N $URL;
+else
+    wget --header="User-Agent: MonTransit" -S $URL;
+fi;
+if [ -e $FILENAME ]; then
+	mv $FILENAME input/gtfs.zip;
+fi;
 echo ">> Downloading... DONE"
